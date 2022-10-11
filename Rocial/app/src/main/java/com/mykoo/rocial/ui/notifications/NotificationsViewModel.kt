4266d27f5892f773f19a9781499c9501c2bc904e
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-10-10 21:34:13 -0400

    Adding files to cloned Repo

---
+++ Rocial/app/src/main/java/com/mykoo/rocial/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.mykoo.rocial.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
